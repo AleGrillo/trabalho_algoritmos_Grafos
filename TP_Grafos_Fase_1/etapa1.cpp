4524d74491d305c2c22bc5c96438685e797887af
Alexandre Gillo <alexandregrill@gmail.com>
2018-05-11 10:02:33 -0300

    main para leitura de arquivo e criação do grafo implementado

--- TP_Grafos_Fase_1/etapa1.cpp
+++ TP_Grafos_Fase_1/etapa1.cpp
@@ -5,11 +5,11 @@
 using namespace std;
 
 struct coord{
-	int x;
-	int y;
+	double x;
+	double y;
 };
 
-typedef int dado; //Para ficar mais fácil substituir o tipo no futuro
+typedef double dado; //Para ficar mais fácil substituir o tipo no futuro
 
 /**
  * Classe noh que guardara as informações de cada vertice do grafo
@@ -1594,7 +1594,132 @@ grafo* geraSubGrafoIA(grafo* G){
 	return NULL;
 }
 
-int main(){
+int menu(){
+	int comando = -1;
+	return comando;
+}
 
+////////////////////////////////////////////////////////////////////////
+//////////////////////////Funções do Main///////////////////////////////
+
+string nomeArq(){
+	cout << "-NOME DO ARQUIVO-\n";
+	string nome;
+	cin >> nome;
+	return nome;
+}
+
+bool ePonderado(){
+	cout << "-GRAFO PONDERADO-\n";
+	cout << "Sim				0\n";
+	cout << "Nao				1\n";
+	int comando;
+	cin >> comando;
+	
+	if(comando == 0){
+		return false;
+	}
+	else if(comando == 1){
+		return true;
+	}
+	else{
+		cout << "comando invalido\n";
+		exit(EXIT_FAILURE);
+	}
+}
+
+int estruturaUtilizada(){
+	cout << "-QUAL ESTRUTURA DE DADOS SERA UTILIZADA-\n";
+	cout << "Lista de Adjacencias					1\n";
+	cout << "Matriz de Adjacencias					2\n";
+	cout << "Matriz de Incidencias					3\n";
+	
+	int comando;
+	cin >> comando;
+	
+	if(comando > 0 and comando < 4){
+		return comando;
+	}
+	else{
+		cout << "comando invalido\n";
+		exit(EXIT_FAILURE);
+	}
+}
+
+bool dadosVertices(){
+	cout << "-QUAIS DADOS SERAO PASSADOS PARA OS VERTICES-\n";
+	cout << "Coordenadas (Euclediano)				0\n";
+	cout << "Apenas valores						1\n";
+	
+	int comando;
+	cin >> comando;
+	
+	if(comando == 0){
+		return false;
+	}
+	else if(comando == 1){
+		return true;
+	}
+	else{
+		cout << "comando invalido\n";
+		exit(EXIT_FAILURE);
+	}
+}
+
+int main(){
+	
+	bool ponderado = ePonderado();
+	bool eucledian = dadosVertices();
+	int qualEstrutura = estruturaUtilizada();
+	string orientation;
+	string nomeArquivo = nomeArq();
+	ifstream arquivo(nomeArquivo.c_str());
+	if(arquivo){
+		arquivo >> orientation;
+		if(orientation == "UNDIRECTED" or orientation == "DIRECTED"){
+			grafo* Grafo = new grafo(orientation, ponderado, qualEstrutura);
+			
+			while(arquivo.good()){
+				if(ponderado == true){
+					dado dadosNohA;
+					dado dadosNohB;
+					int peso;
+					arquivo >> dadosNohA;
+					arquivo >> dadosNohB;
+					arquivo >> peso;
+					Grafo->insertAresta(dadosNohA,dadosNohB,peso);
+				}
+				else{
+					if(eucledian == true){
+						coord coordenadasA;
+						coord coordenadasB;
+						int num;
+						arquivo >> num;
+						arquivo >> coordenadasA.x;
+						arquivo >> coordenadasA.y;
+						arquivo >> coordenadasB.x;
+						arquivo >> coordenadasB.y;
+						Grafo->insertAresta(coordenadasA,coordenadasB);
+					}
+					else{
+						dado dadosNohA;
+						dado dadosNohB;
+						arquivo >> dadosNohA;
+						arquivo >> dadosNohB;
+						Grafo->insertAresta(dadosNohA,dadosNohB);
+					}
+				}
+			}
+		}
+		else{
+			cerr << "falha ao ler o arquivo\n";
+			exit(EXIT_FAILURE);
+		}
+	}
+	else{
+		cerr << "falha ao abrir o arquivo\n";
+		exit(EXIT_FAILURE);
+	}
+	
 return 0;
 }
